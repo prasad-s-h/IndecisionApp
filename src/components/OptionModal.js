e7ddd2ace00ef66a389d5b2841486d69f5563541
prasad-s-h <prasads367@gmail.com>
2019-05-21 22:26:27 +0530

    last

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import Modal from "react-modal";
 
 const OptionModal = (props) =>  (
-      <Modal isOpen={!!props.optionSelected} contentLabel="Selected Option" onRequestClose={props.handleClearSelectedOption}>
-        <h3>Selected Option</h3>
-        {props.optionSelected && <p>{props.optionSelected}</p>}
-        <button onClick={props.handleClearSelectedOption}>okay</button>
+      <Modal 
+       isOpen={!!props.optionSelected} 
+       contentLabel="Selected Option" 
+       onRequestClose={props.handleClearSelectedOption}
+       closeTimeoutMS={200}
+       className="modal"
+      >
+        <h3 className="modal__title">Selected Option</h3>
+        {props.optionSelected && <p className="modal__body">{props.optionSelected}</p>}
+        <button className="button" onClick={props.handleClearSelectedOption}>okay</button>
       </Modal>
     );
 
--- src/components/Options.js
+++ src/components/Options.js
@@ -5,18 +5,24 @@ export default class Options extends React.Component {
   render() {
     return (
       <div>
-        <button
-          className="button button--link" 
-          onClick={this.props.handleDeleteOptions}
-        >RemoveAll</button>
-        {this.props.options.length === 0 && <p>Please provide options</p>}
-        {this.props.options.map(option => (
+        <div className="widget-header">
+          <h3 className="widget-header__title">Your Options</h3>
+          <button
+            className="button button--link"
+            onClick={this.props.handleDeleteOptions}
+          >
+          RemoveAll
+          </button>
+        </div>
+        {this.props.options.length === 0 && <p className="widget__message">Please provide options</p>}
+        {this.props.options.map((option, index) => (
           <Option
             key={option}
             optionText={option}
+            count={index+1}
             handleDeleteOption={this.props.handleDeleteOption}
           />
-        ))}
+        ) )}
       </div>
     );
   }
